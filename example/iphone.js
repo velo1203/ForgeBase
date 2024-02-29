6d8916ac63743d1c6e7aab48f1d67df4c09b2f5b
velo1203 <hokitoki1203@gmail.com>
2024-02-29 16:13:59 +0900

    add bulk sys

--- example/iphone.js
+++ example/iphone.js
@@ -4,12 +4,20 @@ const db = new forgebase("./index.db");
 
 const reference = db.setReference("iphone");
 
-// reference.create({
-//     name: "iphone 12",
-//     price: 1000000,
-//     color: "black",
-// });
+const iphones = [];
 
+// for (let i = 0; i < 1000; i++) {
+//     iphones.push({
+//         name: `iphone ${i}`,
+//         price: i * 1000,
+//         color: "black",
+//     });
+// }
+// for (let i = 0; i < 80; i++) {
+//     reference.create(iphones);
+// }
+const time = Date.now();
 reference.get({ name: "iphone 12" }).then((result) => {
     console.log(result);
+    console.log(Date.now() - time);
 });
